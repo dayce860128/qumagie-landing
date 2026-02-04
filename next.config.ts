d145415e62b68d73231375d757222b41d04924eb
dayce860128 <48014814+dayce860128@users.noreply.github.com>
2026-02-04 23:35:50 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "qumagie-landing";
+
 const nextConfig: NextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
+
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+
   // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  // distDir: "dist",
 };
 
 export default nextConfig;
